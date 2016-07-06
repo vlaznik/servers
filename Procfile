08c918174f2258bc72f369d9d19403cfc2b8be5f
John Doe <johndoe@example.com>
2016-07-06 14:32:17 -0400

    first Heroku commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: ruby server.rb -p $PORT
+git add .
+git commit -m "first Heroku commit"
---
+++ server.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+get '/' do
+  "Hello world"
+end
+
+get "/sinatra" do
+  "Hello Sinatra"
+end
